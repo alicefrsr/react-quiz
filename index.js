4cd66ab0c1929b3218187f0add5f1d07b45340b2
alicefrsr <alicefrsr@gmail.com>
2023-09-23 15:50:26 +0200

    add vercel.json file, rename server.js to index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import cors from 'cors';
+// import cors from 'cors';
 // import bodyParser from 'body-parser';
 import questionsRoute from './routes/questionsRoute.js';
 
 const app = express();
 const PORT = 8000; // cli (mac) $export=PORT=500
-app.use(cors());
+// app.use(cors());
 
 // app.use(bodyParser.json());
 // app.use(express.json());
@@ -13,15 +13,9 @@ app.use(cors());
 app.use('/api/v1/questions', questionsRoute);
 
 app.get('/api/v1', (req, res) => {
-  res
-    .status(200)
-    .send(
-      'Welcome to this very basic Quiz questions API. To view questions, go to http://localhost:8000/api/v1/questions'
-    );
+  res.status(200).send('Welcome to this very basic Quiz questions API');
 });
 
 app.listen(PORT, () => {
-  console.log(
-    `Express server running on http://localhost:${PORT}. Go to /api/v1 for API.`
-  );
+  console.log(`Express server running on port ${PORT}. Go to /api/v1 for API.`);
 });
